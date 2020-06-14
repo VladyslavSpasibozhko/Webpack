b825849428ceb828b91f8a2d8d56f8758b37a393
Vladyslav Spasibozhko <spas_vlad@ukr.net>
2020-06-14 22:22:55 +0300

    setup loaders

--- task1/src/index.js
+++ task1/src/index.js
@@ -1,1 +1,18 @@
-document.body.style.backgroundColor = 'green'
+import './styles/style.scss'
+import './styles/style.less'
+import './styles/style.sass'
+
+import jpegImage from './assets/file.jpg'
+import pngImage from './assets/shrek-001.png'
+import svgImage from './assets/Freesample.svg'
+import gif from './assets/6os.gif'
+
+const say =()=> console.log('Hello')
+
+class Hello {
+  static say = 'hello'
+}
+
+const hello = new Hello()
+console.log(hello)
+say()
--- task1/webpack.config.js
+++ task1/webpack.config.js
@@ -1,9 +1,42 @@
 const path = require('path')
-
 module.exports = {
   entry:'./src/index.js',
   output:{
     filename:'bundle.js',
     path:path.resolve(__dirname, 'dist')
+  },
+  module:{
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: ['style-loader','css-loader', 'less-loader']
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: ['style-loader','css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader:'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      },
+    ]
   }
 }
